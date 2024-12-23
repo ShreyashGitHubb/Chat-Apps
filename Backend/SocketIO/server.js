657a38a568b62954ddf3d91db41d6624b7f56aef
shreyash vishwakarma <shreyasvishwakarma00@gmail.com>
2024-12-23 09:36:18 +0530

    Update server.js

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -1,6 +1,8 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import cors from "cors";
+
 
 const app = express();
 
@@ -9,6 +11,7 @@ const io = new Server(server, {
   cors: {
     origin: "https://shreyas-chat-app.netlify.app",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
